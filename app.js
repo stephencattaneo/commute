0b9c40e407bfe161f5ef241ee27348d7cd82290e
Stephen Cattaneo <scattaneo@airwave.com>
2013-08-19 19:11:34 -0700

    inital commit

---
+++ app.js
@@ -0,0 +1,131 @@
+app = function () {
+  this.data['bart'] = {'south_bound':[{"FTVL":"04:52:00","FRMT":"05:21:00"},{"FTVL":"05:07:00","FRMT":"05:36:00"},{"FTVL":"05:22:00","FRMT":"05:51:00"},{"FTVL":"05:37:00","FRMT":"06:06:00"},{"FTVL":"05:52:00","FRMT":"06:21:00"},{"FTVL":"06:07:00","FRMT":"06:36:00"},{"FTVL":"06:22:00","FRMT":"06:51:00"},{"FTVL":"06:37:00","FRMT":"07:06:00"},{"FTVL":"06:46:00","FRMT":"07:16:00"},{"FTVL":"06:52:00","FRMT":"07:21:00"},{"FTVL":"07:01:00","FRMT":"07:31:00"},{"FTVL":"07:07:00","FRMT":"07:36:00"},{"FTVL":"07:16:00","FRMT":"07:46:00"},{"FTVL":"07:22:00","FRMT":"07:51:00"},{"FTVL":"07:31:00","FRMT":"08:01:00"},{"FTVL":"07:37:00","FRMT":"08:06:00"},{"FTVL":"07:46:00","FRMT":"08:16:00"},{"FTVL":"07:52:00","FRMT":"08:21:00"},{"FTVL":"08:01:00","FRMT":"08:31:00"},{"FTVL":"08:07:00","FRMT":"08:36:00"},{"FTVL":"08:16:00","FRMT":"08:46:00"},{"FTVL":"08:22:00","FRMT":"08:51:00"},{"FTVL":"08:31:00","FRMT":"09:01:00"},{"FTVL":"08:37:00","FRMT":"09:06:00"},{"FTVL":"08:46:00","FRMT":"09:16:00"},{"FTVL":"08:52:00","FRMT":"09:21:00"},{"FTVL":"09:01:00","FRMT":"09:31:00"},{"FTVL":"09:07:00","FRMT":"09:36:00"},{"FTVL":"09:16:00","FRMT":"09:46:00"},{"FTVL":"09:22:00","FRMT":"09:51:00"},{"FTVL":"09:31:00","FRMT":"10:01:00"},{"FTVL":"09:37:00","FRMT":"10:06:00"},{"FTVL":"09:46:00","FRMT":"10:16:00"},{"FTVL":"09:52:00","FRMT":"10:21:00"},{"FTVL":"10:01:00","FRMT":"10:31:00"},{"FTVL":"10:07:00","FRMT":"10:36:00"},{"FTVL":"10:16:00","FRMT":"10:46:00"},{"FTVL":"10:22:00","FRMT":"10:51:00"},{"FTVL":"10:31:00","FRMT":"11:01:00"},{"FTVL":"10:37:00","FRMT":"11:06:00"},{"FTVL":"10:46:00","FRMT":"11:16:00"},{"FTVL":"10:52:00","FRMT":"11:21:00"},{"FTVL":"11:01:00","FRMT":"11:31:00"},{"FTVL":"11:07:00","FRMT":"11:36:00"},{"FTVL":"11:16:00","FRMT":"11:46:00"},{"FTVL":"11:22:00","FRMT":"11:51:00"},{"FTVL":"11:31:00","FRMT":"12:01:00"},{"FTVL":"11:37:00","FRMT":"12:06:00"},{"FTVL":"11:46:00","FRMT":"12:16:00"},{"FTVL":"11:52:00","FRMT":"12:21:00"},{"FTVL":"12:01:00","FRMT":"12:31:00"},{"FTVL":"12:07:00","FRMT":"12:36:00"},{"FTVL":"12:16:00","FRMT":"12:46:00"},{"FTVL":"12:22:00","FRMT":"12:51:00"}], 'north_bound': [{"FTVL":"04:29:00","FRMT":"04:00:00"},{"FTVL":"04:44:00","FRMT":"04:15:00"},{"FTVL":"04:59:00","FRMT":"04:30:00"},{"FTVL":"05:14:00","FRMT":"04:45:00"},{"FTVL":"05:29:00","FRMT":"05:00:00"},{"FTVL":"05:35:00","FRMT":"05:06:00"},{"FTVL":"05:44:00","FRMT":"05:15:00"},{"FTVL":"05:50:00","FRMT":"05:21:00"},{"FTVL":"05:59:00","FRMT":"05:30:00"},{"FTVL":"06:05:00","FRMT":"05:36:00"},{"FTVL":"06:14:00","FRMT":"05:45:00"},{"FTVL":"06:20:00","FRMT":"05:51:00"},{"FTVL":"06:29:00","FRMT":"06:00:00"},{"FTVL":"06:35:00","FRMT":"06:06:00"},{"FTVL":"06:44:00","FRMT":"06:15:00"},{"FTVL":"06:50:00","FRMT":"06:21:00"},{"FTVL":"06:59:00","FRMT":"06:30:00"},{"FTVL":"07:05:00","FRMT":"06:36:00"},{"FTVL":"07:14:00","FRMT":"06:45:00"},{"FTVL":"07:20:00","FRMT":"06:51:00"},{"FTVL":"07:29:00","FRMT":"07:00:00"},{"FTVL":"07:35:00","FRMT":"07:06:00"},{"FTVL":"07:44:00","FRMT":"07:15:00"},{"FTVL":"07:50:00","FRMT":"07:21:00"},{"FTVL":"07:59:00","FRMT":"07:30:00"},{"FTVL":"08:05:00","FRMT":"07:36:00"},{"FTVL":"08:14:00","FRMT":"07:45:00"},{"FTVL":"08:20:00","FRMT":"07:51:00"},{"FTVL":"08:29:00","FRMT":"08:00:00"},{"FTVL":"08:35:00","FRMT":"08:06:00"},{"FTVL":"08:44:00","FRMT":"08:15:00"},{"FTVL":"08:50:00","FRMT":"08:21:00"},{"FTVL":"08:59:00","FRMT":"08:30:00"},{"FTVL":"09:05:00","FRMT":"08:36:00"},{"FTVL":"09:14:00","FRMT":"08:45:00"},{"FTVL":"09:20:00","FRMT":"08:51:00"},{"FTVL":"09:29:00","FRMT":"09:00:00"},{"FTVL":"09:35:00","FRMT":"09:06:00"},{"FTVL":"09:44:00","FRMT":"09:15:00"},{"FTVL":"09:50:00","FRMT":"09:21:00"},{"FTVL":"09:59:00","FRMT":"09:30:00"},{"FTVL":"10:05:00","FRMT":"09:36:00"},{"FTVL":"10:14:00","FRMT":"09:45:00"},{"FTVL":"10:20:00","FRMT":"09:51:00"},{"FTVL":"10:29:00","FRMT":"10:00:00"},{"FTVL":"10:35:00","FRMT":"10:06:00"},{"FTVL":"10:44:00","FRMT":"10:15:00"},{"FTVL":"10:50:00","FRMT":"10:21:00"},{"FTVL":"10:59:00","FRMT":"10:30:00"},{"FTVL":"11:05:00","FRMT":"10:36:00"},{"FTVL":"11:14:00","FRMT":"10:45:00"},{"FTVL":"11:20:00","FRMT":"10:51:00"},{"FTVL":"11:29:00","FRMT":"11:00:00"},{"FTVL":"11:35:00","FRMT":"11:06:00"},{"FTVL":"11:44:00","FRMT":"11:15:00"},{"FTVL":"11:50:00","FRMT":"11:21:00"},{"FTVL":"11:59:00","FRMT":"11:30:00"},{"FTVL":"12:05:00","FRMT":"11:36:00"},{"FTVL":"12:14:00","FRMT":"11:45:00"},{"FTVL":"12:20:00","FRMT":"11:51:00"},{"FTVL":"12:29:00","FRMT":"12:00:00"},{"FTVL":"12:35:00","FRMT":"12:06:00"},{"FTVL":"12:44:00","FRMT":"12:15:00"},{"FTVL":"12:50:00","FRMT":"12:21:00"},{"FTVL":"12:59:00","FRMT":"12:30:00"}]};
+  this.data['vta'] = {'south_bound':[{"FRMT":"6:12","SNYV":"6:52"},{"FRMT":"7:12","SNYV":"7:58"},{"FRMT":"7:43","SNYV":"8:32"},{"FRMT":"7:58","SNYV":"8:47"},{"FRMT":"8:13","SNYV":"9:02"},{"FRMT":"8:28","SNYV":"9:17"}],'north_bound':[{"SNYV":"16:05","FRMT":"16:57"},{"SNYV":"16:37","FRMT":"17:34"},{"SNYV":"17:05","FRMT":"18:06"},{"SNYV":"17:25","FRMT":"18:24"},{"SNYV":"17:44","FRMT":"18:43"},{"SNYV":"18:13","FRMT":"19:10"}]};
+
+  this.leg_pointers = {
+    'bart': 0, 'vta': 0
+  };
+};
+
+app.prototype.data = {};
+app.prototype.direction = '';
+app.prototype.first_leg = '';
+app.prototype.second_leg = '';
+app.prototype.now = null;
+app.prototype.leg_pointers = {};
+
+app.prototype.run = function () {
+  this.now = new Date();
+  var noon = (new Date()).setHours(12);
+  var direction, first_leg, second_leg;
+  if (this.now < noon) {
+    this.direction = 'south_bound';
+    this.first_leg = 'bart';
+    this.second_leg = 'vta';
+  } else {
+    this.direction = 'north_bound';
+    this.first_leg = 'vta';
+    this.second_leg = 'bart';
+  }
+
+  var first_leg_options = [];
+  var second_leg_options = {};
+  for (var i=0; i < 2; i++) {
+    var option = this.getNextEarliestPosition(this.now, this.first_leg);
+    if (! option) break;
+    first_leg_options.push(option);
+
+    var end = this.getEnd('first_leg');
+    second_leg_options[option[end]] = this.getNextEarliestPosition(option[end], this.second_leg);
+  }
+
+  this.display(first_leg_options, second_leg_options);
+};
+
+
+/**
+ * @param {Array.<Ojbect>} first_leg_options The next couple options for the 
+ *  first leg of the commute
+ * @param {Array.<Object>} second_leg_options Based on the first leg options
+ */
+app.prototype.display = function(first_leg_options, second_leg_options) {
+  console.log('display');
+};
+
+
+/**
+ * @param {string} leg The leg of the commmute.
+ * @return {string} The name of the ending station.
+ */
+app.prototype.getEnd = function(leg) {
+  if (this.direction === 'south_bound') {
+    if (leg === 'vta') return 'SNYV';
+  } else {
+    if (leg === 'bart') return 'FTVL';
+  }
+
+  return 'FRMT';
+};
+
+
+/**
+ * @param {string} leg The leg of the commmute.
+ * @return {string} The name of the starting station.
+ */
+app.prototype.getStart = function(leg) {
+  if (this.direction === 'north_bound') {
+    if (leg === 'vta') return 'SNYV';
+  } else {
+    if (leg === 'bart') return 'FTVL';
+  }
+
+  return 'FRMT';
+};
+
+
+/**
+ * @param {string} string_date A string representing Hours and Minutes.
+ * @return {Date} Sometime today with hours and minutes based on the input.
+ */
+app.prototype.dateifyString = function (string_date) {
+  var d = new Date();
+  var date_parts = string_date.split(':');
+  d.setHours(date_parts[0]);
+  d.setMinutes(date_parts[1]);
+  return d;
+};
+
+
+/**
+ * @param {Date} after The next time must follow this time.
+ * @param {string} leg Specifies what lef of a trip.
+ * @return {!Object} The next leg option given the provided inputs.
+ */
+app.prototype.getNextEarliestPosition = function(after, leg) {
+  var list = this.data[leg][this.direction];
+  var start = this.getStart(leg);
+  var end = this.getEnd(leg);
+
+  var item = {};
+  item[start] = this.getNow()
+  item[start].setDate(this.now.getDate() - 1);
+  var pointer = this.leg_pointers[leg];
+
+  while (item[start] < after) {
+    item = list[pointer];
+    if (! item) break;
+
+    item[start] = this.dateifyString(item[start]);
+  }
+
+  item[end] = this.dateifyString(item[end]);
+  return item;
+};
+
+/**
+ * This method is only for testing.
+ * @return {Date} A date object representing now.
+ */ 
+app.prototype.getNow = function() {
+  return new Date();
+};
---
+++ parse_stops.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+from collections import defaultdict as dd
+import json
+from datetime import time
+
+input_file = 'bart_stop_times.txt'
+output_file = 'out.json'
+data = dd(dict)
+
+class TimeFriendlyEncoder(json.JSONEncoder):
+  def default(self, obj):
+    if type(obj) == time:
+      return obj.strftime('%H:%M:%S')
+    else:
+      return json.JSONEncoder.default(obj)
+
+
+def run():
+  fd = open(input_file)
+  for l in fd.readlines():
+    trip_id,arrival_time,departure_time,stop_id,stop_sequence,stop_headsign,pickup_type,drop_off_type = l.split(',')  
+
+    if 'SAT' in trip_id or 'SUN' in trip_id:
+      continue
+
+    if stop_id  == 'FTVL' or stop_id == 'FRMT':
+      try:
+        t = [int(x) for x in arrival_time.split(':')]
+        if t[0] > 12: continue
+        
+        data[trip_id][stop_id] = time(*t)
+      except ValueError, e:
+        pass #we don't care about midnight or 1 am trains
+
+  fd.close()
+
+  north_bound = []
+  south_bound = []
+
+  for key in data:
+    if len(data[key]) == 2:
+      if data[key]['FTVL'] > data[key]['FRMT']:
+        north_bound.append(data[key])
+      else:
+        south_bound.append(data[key])
+
+  north_bound.sort(cmp=lambda x, y: -1 if x['FRMT'] < y['FRMT'] else 1)
+  south_bound.sort(cmp=lambda x, y: -1 if x['FTVL'] < y['FTVL'] else 1)
+
+  fd = open(output_file, 'w')
+  json.dump({'north_bound': north_bound, 'south_bound': south_bound}, fd, cls=TimeFriendlyEncoder)
+  fd.close()
+
+if __name__ == '__main__':
+  run()
